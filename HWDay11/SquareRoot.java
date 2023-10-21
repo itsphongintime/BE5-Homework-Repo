5ac39fb81a4abb485f79c56c507d9a78c5691bbf
Viet Phong Nguyen <104746173+itsphongintime@users.noreply.github.com>
2023-10-22 00:53:34 +1030

    Update SquareRoot.java

--- HWDay11/SquareRoot.java
+++ HWDay11/SquareRoot.java
@@ -1,7 +1,7 @@
 package HWDay11;
 
 public class SquareRoot {
-	public static int sqrt(int n) {
-		return (int) Math.pow(n, 0.5);
+	public static double sqrt(int n) {
+		return Math.pow(n, 0.5);
 	} // O(1)
 }
