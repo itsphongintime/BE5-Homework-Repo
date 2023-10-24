c6223043f4661367f3928bc96aad98bb5f458a39
Viet Phong Nguyen <104746173+itsphongintime@users.noreply.github.com>
2023-10-24 21:29:44 +1030

    Add files via upload

--- HWDay11/RomanNumber.java
+++ HWDay11/RomanNumber.java
@@ -2,11 +2,15 @@ package HWDay11;
 
 public class RomanNumber {
 	public static int romanToInt (String s) {
-		// Initialize result
 		int result = 0;
-		
-		// Convert the rightmost char to integer and add it to result
-		switch (s.charAt(s.length() - 1)) { // O(1)
+		result = convertRightmostCharToInt(result, s.charAt(s.length() - 1));
+		result = convertRomanStringToInt(result, s);
+		return result;
+	} // Time complexity O(1) + O(n - 1) = O(n)
+	
+	// Convert the rightmost char to integer and add it to result
+	public static int convertRightmostCharToInt (int result, char rightmostChar) { // O(1)
+		switch (rightmostChar) {
 		case 'I':
 			result++;
 			break;
@@ -35,7 +39,10 @@ public class RomanNumber {
 			result += 1000;
 			break;
 		}
-		
+		return result;
+	}
+	
+	public static int convertRomanStringToInt(int result, String s) {
 		// Iterate from Right to Left of char array
 		for (int i = s.length() - 2; i >= 0; i--) { // O(n - 1) where n is length of string
 			// Add the integer value of each Roman letter to the result.
@@ -46,33 +53,33 @@ public class RomanNumber {
 					result -= 2;
 				} // Subtraction instance for 'I'
 				break;
-				
+						
 			case 'V':
 				result += 5;
 				break;
-				
+						
 			case 'X':
 				result += 10;
 				if (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C') {
 					result -= 20;
 				} // Subtraction instance for 'X'
 				break;
-				
+						
 			case 'L':
 				result += 50;
 				break;
-				
+						
 			case 'C':
 				result += 100;
 				if (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M') {
 					result -= 200;
 				} // Subtraction instance for 'C'
 				break;
-				
+						
 			case 'D':
 				result += 500;
 				break;
-				
+						
 			case 'M':
 				result += 1000;
 				break;
@@ -80,4 +87,5 @@ public class RomanNumber {
 		}
 		return result;
 	}
-} // Time complexity O(1) + O(n - 1) = O(n)
+} 
+
--- HWDay11/SquareRoot.java
+++ HWDay11/SquareRoot.java
@@ -1,7 +1,28 @@
 package HWDay11;
 
 public class SquareRoot {
+	public static void main(String[] args) {
+		System.out.println(sqrt(17));
+	}
+	
 	public static double sqrt(int n) {
-		return Math.pow(n, 0.5);
-	} // O(1)
-}
+		int result = 0;
+		
+		while (result*result < n) {
+			result++;
+		}
+			
+		for (int i = 0; i < 3; i++) {
+			result = result - ((result*result) - n)/(2*result);
+		}
+		return result;
+	}
+} 
+// O(n)
+// Code này dùng Newton Raphson method. E dùng loop đầu để tính số mà khi nhân với chính nó bé hơn và gần với input nhất.
+// Ví dụ: input là 15, thì loop đầu này sẽ return 3 vào result, vì 3*3 = 9 bé hơn và sát với 15 nhất. Mặc dù 4*4 = 16 gần 15 hơn, nhưng 16 > 15 nên ko đc.
+// Thì n ở đây là số loop chạy để tìm ra cái số này.
+
+// Sau đó, dùng công thức ở loop dưới để tìm bên thập phân. Số loop phụ thuộc vào error margin. Đề này thầy cho error +- 0.01 thì e cho code chạy 3 lần. 
+// Nếu error là 0.001 thì e tăng số loop dưới lên 4 và các error margin khác cũng tương tự.
+// Cái này cơ bản là O(1).
