fc3dedef6df6a1e15b6c2e6ae3f17a8af6f912f1
Viet Phong Nguyen <104746173+itsphongintime@users.noreply.github.com>
2023-12-20 19:00:42 +1030

    Delete SQLIntergration/Database.java

--- SQLIntergration/Database.java
+++
@@ -1,9 +0,0 @@
-package SQLIntergration;
-
-import java.util.ArrayList;
-
-public class Database {
-	public static ArrayList<Account> accountDatabase = new ArrayList<Account>();
-	public static ArrayList<Course> courseDatabase = new ArrayList<Course>();
-	public static ArrayList<Mentor> mentorDatabase= new ArrayList<Mentor>();
-}
