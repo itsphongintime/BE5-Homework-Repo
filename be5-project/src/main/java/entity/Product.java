62faf33591e2738070cf743f275a7a208be4bc7e
Viet Phong Nguyen <104746173+itsphongintime@users.noreply.github.com>
2023-12-28 21:05:39 +1030

    Add files via upload

---
+++ be5-project/src/main/java/entity/Product.java
@@ -0,0 +1,88 @@
+package entity;
+
+public class Product {
+	private int id;
+	private String name;
+	private int price;
+	private String imgName;
+	private boolean isNew;
+	private int quantity;
+	private String description;
+	
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Product(int id, String name, int price, String imgName, boolean isNew, int quantity, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.imgName = imgName;
+		this.isNew = isNew;
+		this.quantity = quantity;
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getImgName() {
+		return imgName;
+	}
+
+	public void setImgName(String imgName) {
+		this.imgName = imgName;
+	}
+
+
+	public int getid() {
+		return id;
+	}
+
+
+	public void setid(int id) {
+		this.id = id;
+	}
+
+
+	public boolean isNew() {
+		return isNew;
+	}
+
+
+	public void setNew(boolean isNew) {
+		this.isNew = isNew;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
